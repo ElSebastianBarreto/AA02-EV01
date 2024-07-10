9625d2b05b4e7427ebcc32bb3f970cdff05197c5
Sebastian Barreto <sebastian.barreto2002@gmail.com>
2024-07-10 18:35:45 -0500

    Implementacion de registro de prueba

--- src/App.java
+++ src/App.java
@@ -6,10 +6,32 @@ public class App {
         String dbUrl = "jdbc:postgresql://localhost:5432/prueba";
         String user = "postgres";
         String clave = "root";
+
+         class Cliente {
+
+            int Cedula; 
+            String Nombre; 
+            String Apellido;
+            int Edad; 
+            String Telefono;
+            String Correo; 
+            String Clave; 
+         }
     
         try (Connection conn = DriverManager.getConnection(dbUrl, user, clave);
              Statement stmt = conn.createStatement();
              ) {
+
+
+
+
+
+
+
+
+
+
+
         } catch (SQLException e) {
             e.printStackTrace();
         }
