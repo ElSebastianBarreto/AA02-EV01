dc4ee7705127b60d59c0d742f0ec69ddc2826b45
Sebastian Barreto <sebastian.barreto2002@gmail.com>
2024-07-10 17:55:06 -0500

    conexion con base de datos postgres

--- src/App.java
+++ src/App.java
@@ -1,5 +1,17 @@
+import java.sql.*;
+
 public class App {
-    public static void main(String[] args) throws Exception {
-        System.out.println("Hello, Woorld!");
+    public static void main(String[] args) {
+     
+        String dbUrl = "jdbc:postgresql://localhost:5432/prueba";
+        String user = "postgres";
+        String clave = "root";
+    
+        try (Connection conn = DriverManager.getConnection(dbUrl, user, clave);
+             Statement stmt = conn.createStatement();
+             ) {
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
     }
 }
