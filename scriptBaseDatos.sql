90f14cc1fe194c443d16ac743a609b25821d0645
Sebastian Barreto <sebastian.barreto2002@gmail.com>
2024-07-10 18:34:08 -0500

    creacion de un registro de prueba

--- scriptBaseDatos.sql
+++ scriptBaseDatos.sql
@@ -2,8 +2,11 @@ CREATE TABLE IF NOT EXISTS Cliente (
   Cedula INT NOT NULL,
   Nombre VARCHAR(70) NOT NULL,
   Apellido VARCHAR(70) NOT NULL,
-   Edad INT NOT NULL,
-  Telefono INT NOT NULL,
+  Edad INT NOT NULL,
   Correo VARCHAR(100) NOT NULL,
   Clave VARCHAR(100) NOT NULL,
+  Telefono VARCHAR(13) NOT NULL,
   PRIMARY KEY (Cedula));
+
+a
+  insert into cliente VALUES (123, 'Sebastian', 'Barreto', 24, 'sebastianqgmail.com', 'clave', '3214186414');
